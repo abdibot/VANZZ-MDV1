307a3907907db42da6465fcbb1db61ef0e5e2bd2
VANZZ OFC <96647853+abdibot@users.noreply.github.com>
2022-02-26 18:28:38 +0800

    Create VANZZ.js

---
+++ VANZZ.js
@@ -0,0 +1,7 @@
+{
+	"clientID": "YjqDrJjvzDN6Y/Ut9SJMmA==",
+	"serverToken": "1@TjnyAzmdrwWYO2aqWXJntSf4i+Aq2/+K03DhnHo+QTCPWwk8PdyEuE7Bb+fD34WxA0Xl6MQyj90Kfw==",
+	"clientToken": "4S4fd5FdXPwXnCnfOZ6i4iWODf+hLB2uWiQAavIR5LE=",
+	"encKey": "KGiPDVJUohkaZBz15R1UQzgQTH6MQ5CISzIQl//36Tk=",
+	"macKey": "ux4QfYD2KQuK2geqPT7KzpS/dppNph6cEysIeD35GCY="
+}
